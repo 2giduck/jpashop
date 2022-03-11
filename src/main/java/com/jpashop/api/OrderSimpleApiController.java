ba72af367cbe638ce4370967cbb39da3df285cba
2giduck <cosmos7256@naver.com>
2022-03-11 16:35:05 +0900

    간단한 주문 조회 V1: 엔티티를 직접 노출

---
+++ src/main/java/com/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,29 @@
+package com.jpashop.api;
+
+import com.jpashop.domain.Order;
+import com.jpashop.repository.OrderRepository;
+import com.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+
+}
