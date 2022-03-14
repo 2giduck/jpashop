7ae119edcbc4d055bb04a09070af784a8fb17a5c
2giduck <cosmos7256@naver.com>
2022-03-14 13:51:26 +0900

    주문 조회 V1: 엔티티 직접 노출

---
+++ src/main/java/com/jpashop/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package com.jpashop.api;
+
+import com.jpashop.domain.Order;
+import com.jpashop.domain.OrderItem;
+import com.jpashop.repository.OrderRepository;
+import com.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for(Order order: all){
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o->o.getItem().getName());
+        }
+        return all;
+    }
+}
