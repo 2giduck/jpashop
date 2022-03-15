b123cea9e208a5722e684b81cb5c9ee4fdc51559
Dulgiduck <jsh@sngy.io>
2022-03-15 15:50:31 +0900

    주문 조회 V4: JPA에서 DTO 직접 조회

--- src/main/java/com/jpashop/api/OrderApiController.java
+++ src/main/java/com/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import com.jpashop.domain.OrderItem;
 import com.jpashop.domain.OrderStatus;
 import com.jpashop.repository.OrderRepository;
 import com.jpashop.repository.OrderSearch;
+import com.jpashop.repository.order.query.OrderQueryDto;
+import com.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -63,6 +66,12 @@ public class OrderApiController {
                 .collect(Collectors.toList());
 
         return collect;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(@RequestParam(value="offset", defaultValue = "0") int offset,
+                                        @RequestParam(value="limit", defaultValue = "100") int limit){
+        return orderQueryRepository.findOrderQueryDtos();
     }
 
     @Getter
